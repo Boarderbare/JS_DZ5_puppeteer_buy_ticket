d5dc007750c091a8a3dc58972fa65ea583c39337
Sergio <boarderbare@gmail.com>
2022-11-24 18:03:08 +0400

    add files

--- book.test.js
+++ book.test.js
@@ -1,15 +1,13 @@
-const { clickElement } = require("./lib/commands.js");
-const { checkSelectedFilm, checkSelectingTickets, checkBuying, checkSeats } = require("./lib/util.js");
+const { clickElement, selectPlace, selectSeance } = require("./lib/commands.js");
+const { checkSelectedFilm, checkSelectingTickets, checkBuying, checkSeats} = require("./lib/util.js");
 
 let page;
 
 beforeEach(async () => {
   page = await browser.newPage();
-  await page.goto("http://qamid.tmweb.ru/client/index.php");
-  await clickElement(page, "body > nav > a:nth-child(2)"); // выбор даты
-  await clickElement(page, "[data-seance-start='1140']", {
-    timeout: 6000,
-  }); // выбор сеанса
+  await page.goto("http://qamid.tmweb.ru/client/index.php",
+  {setTimeout: 20000},
+  );
 });
 
 afterEach(() => {
@@ -18,14 +16,15 @@ afterEach(() => {
 
 describe("qamid.tmweb.ru tests", () => {
   test("Buying one ticket", async () => {
-    const row = 7;
+    const row = 8;
     const seat = 8;
     const place = row + "/" + seat;
+    const day = 2; 
+    const time = "1140";
 
-    const ticket1 = `div.buying-scheme > div.buying-scheme__wrapper > div:nth-child(${row}) > span:nth-child(${seat})`;
-    await clickElement(page, ticket1);
+    await selectSeance(page, day,time);
+    await selectPlace(page, row, seat);
     await checkSelectedFilm(page);
-
     await clickElement(page, ".acceptin-button");
     await checkSelectingTickets(page);
     await checkSeats(page, place);
@@ -39,14 +38,13 @@ describe("qamid.tmweb.ru tests", () => {
     const seat2 = 3;
     const seat3 = 4;
     const place = row + "/" + seat1 + ", " + row + "/" + seat2 + ", " + row + "/" + seat3;
+    const day = 2; 
+    const time = "1140";
 
-    const ticket1 = `div.buying-scheme > div.buying-scheme__wrapper > div:nth-child(${row}) > span:nth-child(${seat1})`;
-    await clickElement(page, ticket1);
-    const ticket2 = `div.buying-scheme > div.buying-scheme__wrapper > div:nth-child(${row}) > span:nth-child(${seat2})`;
-    await clickElement(page, ticket2);
-    const ticket3 = `div.buying-scheme > div.buying-scheme__wrapper > div:nth-child(${row}) > span:nth-child(${seat3})`;
-    await clickElement(page, ticket3);
-
+    await selectSeance(page, day,time);
+    await selectPlace(page, row, seat1);
+    await selectPlace(page, row, seat2);
+    await selectPlace(page, row, seat3);
     await checkSelectedFilm(page);
     await clickElement(page, ".acceptin-button");
     await checkSelectingTickets(page);
@@ -59,9 +57,12 @@ describe("qamid.tmweb.ru tests", () => {
     const row = 4;
     const seat = 1;
     const place = row + "/" + seat;
+    const day = 2; 
+    const time = "1140";
 
-    const ticket1 = `div.buying-scheme > div.buying-scheme__wrapper > div:nth-child(${row}) > span:nth-child(${seat})`;
-    await clickElement(page, ticket1);
+    await selectSeance(page, day,time);
+
+    await selectPlace(page, row, seat);
     await checkSelectedFilm(page);
     await clickElement(page, ".acceptin-button");
     await checkSelectingTickets(page);
@@ -71,14 +72,10 @@ describe("qamid.tmweb.ru tests", () => {
 
     //выбор того же места
     await page.goto("http://qamid.tmweb.ru/client/index.php");
-    await clickElement(page, "body > nav > a:nth-child(2)");
-    await clickElement(page, "[data-seance-start='1140']", {
-      timeout: 6000,
-    });
-    await clickElement(page, ticket1);
-    const button_state = String(
-      await page.$eval("button", (button) => {return button.disabled})
-      );
-    expect(button_state).toContain("true");
+    await selectSeance(page, day,time);
+    await selectPlace(page, row, seat);
+
+    const button_state = await page.$eval("button", (button) => {return button.disabled});
+    expect(button_state).toBe(true);
   });
 });
